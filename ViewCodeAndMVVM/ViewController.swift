e960865a9fc9970086d147715536f5be5e089754
personal <filipe.boeck@hotmail.com>
2022-12-03 10:14:22 -0300

    criacao do projeto inicial

---
+++ ViewCodeAndMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewCodeAndMVVM
+//
+//  Created by Filipe Wolf Boeck on 03/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
